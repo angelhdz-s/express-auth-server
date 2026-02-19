3472b645b865da3933e62c8ec64dd2eeba9d4ee4
angelhdz-s <angelhdzsotelo@gmail.com>
2026-02-19 14:54:23 -0600

    add typescript and add index.ts

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
