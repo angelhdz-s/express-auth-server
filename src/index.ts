ef559198a54e9c96cff97eeb5cdb3274ef539c1f
angelhdz-s <angelhdzsotelo@gmail.com>
2026-02-21 16:54:23 -0600

    feat(api): add cors and add json format validation middleware for request body.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "express";
 import { connectDB } from "./config/db.js";
 import { Failure, Success } from "./utils/result.js";
 
@@ -9,6 +10,20 @@ const PORT = 3001;
 await connectDB();
 
 app.use(express.json());
+app.use(cors());
+
+app.use((err: any, _req: any, res: any, next: any) => {
+	if (err instanceof SyntaxError && "body" in err) {
+		return res.status(400).json(
+			Failure({
+				code: 400,
+				message: "Invalid JSON format",
+			}),
+		);
+	}
+
+	next(err);
+});
 
 app.get("/", (_req, res) => {
 	res.json(
