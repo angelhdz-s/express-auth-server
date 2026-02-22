494c6232b6fa1cd316bc2b5b63edb09bda99d1b1
angelhdz-s <angelhdzsotelo@gmail.com>
2026-02-21 18:39:08 -0600

    refactor(seed): add SALT constants when inserting users at user's seed

--- src/seeds/user.seed.ts
+++ src/seeds/user.seed.ts
@@ -1,36 +1,38 @@
 import "dotenv/config";
 import { connectDB } from "../config/db.js";
-import { User } from "../models/user.js";
+import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { SALT } from "../config/constants.js";
 
 export async function seedUsers() {
 	try {
 		await connectDB();
 
 		await User.deleteMany();
+		console.log("Users deleted");
 
 		await User.insertMany([
 			{
 				email: "angelhdz@gmail.com",
 				name: "Angel Hernandez",
-				password: await bcrypt.hash("123456", 10),
+				password: await bcrypt.hash("123456", SALT),
 				username: "angel",
 			},
 			{
 				email: "sergio@gmail.com",
-				password: await bcrypt.hash("123456", 10),
+				password: await bcrypt.hash("123456", SALT),
 				username: "sergio",
 				name: "Sergio Sánchez",
 			},
 			{
 				email: "mario@gmail.com",
-				password: await bcrypt.hash("123456", 10),
+				password: await bcrypt.hash("123456", SALT),
 				username: "mario",
 				name: "Mario Vela",
 			},
 			{
 				email: "barras@gmail.com",
-				password: await bcrypt.hash("123456", 10),
+				password: await bcrypt.hash("123456", SALT),
 				username: "barras",
 				name: "Barras",
 			},
