dd1ae8d5a8bd2fa2d29b88d747ac59658a64740b
angelhdz-s <angelhdzsotelo@gmail.com>
2026-02-22 00:06:15 -0600

    feat(config): add secret, is production and token key constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,4 @@
 export const SALT = 10;
+export const SECRET_JWT_KEY = process.env.SECRET;
+export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const TOKEN_KEY = "access_token";
