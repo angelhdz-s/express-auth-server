dd8d8128c1fdae5bc73a3418ba958c93eafc354c
angelhdz-s <angelhdzsotelo@gmail.com>
2026-02-21 18:41:54 -0600

    feat(api): add user register endpoint validation
    Verify body props
    Validate already existing email and/or username
    Add user insertion into the dabatabe

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import cors from "express";
 import bcrypt from "bcrypt";
 import { connectDB } from "./config/db.js";
 import { Failure, Success } from "./utils/result.js";
-import { User } from "./models/user.js";
+import { User } from "./models/user.model.js";
+import { SALT } from "./config/constants.js";
 
 const app = express();
 const PORT = 3001;
@@ -92,12 +93,53 @@ app.post("/login", async (req, res) => {
 	);
 });
 
-app.post("/register", (_req, res) => {
-	res.json(
+app.post("/register", async (req, res) => {
+	if (
+		!req.body ||
+		!req.body.username ||
+		!req.body.name ||
+		!req.body.email ||
+		!req.body.password
+	)
+		return res.json(
+			Failure({
+				code: 400,
+				message: "Missing body {name} {username} {email} {password}",
+			}),
+		);
+
+	const { name, email, password } = req.body;
+	const username = req.body.username.trim().toLowerCase();
+
+	const usernameExists = await User.findOne({ username });
+	const emailExists = await User.findOne({ email });
+
+	if (usernameExists || emailExists) {
+		let message = `Username {${username}} and email {${email}} already exist`;
+		if (!emailExists) message = `Username {${username}"} already exists`;
+		if (!usernameExists) message = `Email {${email}} already exists`;
+		return res.status(400).json(
+			Failure({
+				code: 400,
+				message,
+			}),
+		);
+	}
+
+	const passwordHash = bcrypt.hashSync(password, SALT);
+
+	const newUser = await User.insertOne({
+		username,
+		name,
+		email,
+		password: passwordHash,
+	});
+
+	return res.status(200).json(
 		Success({
 			code: 200,
-			message: "Index",
-			data: null,
+			message: "User created successfuly",
+			data: newUser,
 		}),
 	);
 });
