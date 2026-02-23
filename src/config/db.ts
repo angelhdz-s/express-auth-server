afbf01d9ff1a71a8bc750633ea09464acfce438f
angelhdz-s <angelhdzsotelo@gmail.com>
2026-02-22 18:11:33 -0600

    feat(db): add db disconnection function

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 const URI = process.env.MONGO_URI ?? "";
 
-console.log(`URI: ${URI}`);
-console.log(``);
-
 export const connectDB = async () => {
 	try {
 		await mongoose.connect(URI);
-		console.log("MongoDB conectado ✅");
+		console.log("MongoDB conectado.");
 	} catch (error) {
 		console.error("Error de conexión a MongoDB:", error);
 		process.exit(1);
+	}
+};
+export const disconnectDB = async () => {
+	try {
+		await mongoose.disconnect();
+		console.log("MongoDB desconectado.");
+	} catch (error) {
+		console.error("Error al desconectar de MongoDB:", error);
 	}
 };
